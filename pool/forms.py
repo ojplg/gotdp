37f9cf6bae9bef2301710fb6be954d0f23750a8b
ojplg <lees@ripco.com>
2019-01-23 17:55:06 -0600

    More form work

--- pool/forms.py
+++ pool/forms.py
@@ -2,5 +2,13 @@ from django import forms
 
 from .models import Character
 
+#    characters = forms.ModelMultipleChoiceField(Character.objects.order_by('name'))
+
 class CharacterSelectsForm(forms.Form):
-    characters = forms.ModelMultipleChoiceField(Character.objects.order_by('name'))
+    def __init__(self, *args, **kwargs):
+        super(CharacterSelectsForm, self).__init__(*args, **kwargs)
+        CHOICES = (('Lives','Lives'),('Dies','Dies'))
+        for c in Character.objects.order_by('name'):
+            self.fields[c.name] = forms.ChoiceField(widget=forms.RadioSelect, choices=CHOICES)
+
+    
