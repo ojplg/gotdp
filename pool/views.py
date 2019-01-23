a29d0463bf7637004986b3b4edf72918180642b2
ojplg <lees@ripco.com>
2019-01-23 12:26:58 -0600

    Add characters to list display

--- pool/views.py
+++ pool/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Character
+
 def index(request):
     return HttpResponse("GOT Death Pool Index")
 
 def select_characters(request):
-    context = {}
+    character_list = Character.objects.order_by('name')
+    context = {'character_list':character_list}
     return render(request,'select_characters.html',context) 
