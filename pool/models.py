369375bf0b54ef7a54e440520df9c087cafdbc97
ojplg <lees@ripco.com>
2019-01-28 23:06:24 -0600

    More models for saving selections

--- pool/models.py
+++ pool/models.py
@@ -6,3 +6,12 @@ class Character(models.Model):
     def __str__(self):
         return 'Character: ' + self.name
 
+class Selection(models.Model):
+    OUTCOMES = (
+        ('L','Lives'),
+        ('D','Dies'))
+    character = models.OneToOneField(Character,on_delete=models.CASCADE)
+    outcome = models.CharField(choices=OUTCOMES,max_length=1)
+
+class Selections(models.Model):
+    picks = models.ManyToManyField(Selection, verbose_name="list of selections")
